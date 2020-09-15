dd1932932fff47d254d320e8c10b25dc313ce450
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-15 12:12:31 +0200

    Added Messenger Logo

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ export default function App() {
 
   return (
     <div className="App">
+      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" />
       <h1>Messenger</h1>
       <h2>Welcome {username}</h2>
       <form>
